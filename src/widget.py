c15329e4dee213f4f56c4efc7e499c71ccd11ecc
Stepan <stepstepan2@gmail.com>
2024-04-24 05:39:52 +0300

    all func

--- src/widget.py
+++ src/widget.py
@@ -16,22 +16,23 @@ def mask_card_and_check(number: str) -> str:
             list_data.append(i)
     word = "".join(list_data).strip()
 
-    if bool(re.search('[а-яА-Я]', list_data[0])):
+    if bool(re.search("[а-яА-Я]", list_data[0])):
 
-        check_mask_account = mask_check(''.join(list_number))
+        check_mask_account = mask_check("".join(list_number))
 
-        return f'{word} {check_mask_account}'
+        return f"{word} {check_mask_account}"
 
     else:
-        card_mask_account = mask_card(''.join(list_number))
+        card_mask_account = mask_card("".join(list_number))
+        name_data = "".join(list_data)
 
-        return f'{''.join(list_data)} {card_mask_account}'
+        return f"{name_data} {card_mask_account}"
 
 
-def time_correct(mask_time : str) -> str:
+def time_correct(mask_time: str) -> str:
     """расшифровывает маску"""
-    list_time = mask_time.split('T')
-    string_time = ''.join(list_time[0])
-    list_time_correct = string_time.split('-')
-    data = '.'.join(list_time_correct[::-1])
+    list_time = mask_time.split("T")
+    string_time = "".join(list_time[0])
+    list_time_correct = string_time.split("-")
+    data = ".".join(list_time_correct[::-1])
     return data
---
+++ tests/main.py
@@ -0,0 +1,6 @@
+from src.widget import mask_card_and_check, time_correct
+
+user_account = input()
+user_data = input()
+print(mask_card_and_check(user_account))
+print(time_correct(user_data))

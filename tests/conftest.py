462164936d8aec821ef9604c892b2de576d4af58
Stepan <stepstepan2@gmail.com>
2024-05-07 12:21:34 +0300

    add black

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,25 +3,29 @@ import pytest
 
 @pytest.fixture
 def card():
-    return '7000792289606361'
+    return "7000792289606361"
 
 
 @pytest.fixture
 def check():
-    return '73654108430135874305'
+    return "73654108430135874305"
 
 
 @pytest.fixture
 def list_sorting():
-    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
 @pytest.fixture
 def list_time_date():
-    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,33 +1,30 @@
-import pytest
 from src.processing import sorting_dict_reverse, sorting_dict_status
 
 
 def test_sorting_dict_status(list_sorting):
-    assert sorting_dict_status(list_sorting, status='EXECUTED') == [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-    assert sorting_dict_status(list_sorting, status='CANCELED') == [
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    assert sorting_dict_status(list_sorting, status="EXECUTED") == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+    assert sorting_dict_status(list_sorting, status="CANCELED") == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
-def test_sorting_dict_reverse(list_time_date, ):
+def test_sorting_dict_reverse(
+    list_time_date,
+):
     assert sorting_dict_reverse(list_time_date, flag=True) == [
-        {'id': 41428829, 'state': 'EXECUTED',
-         'date': '2019-07-03T18:35:29.512364'},
-        {'id': 615064591, 'state': 'CANCELED',
-         'date': '2018-10-14T08:21:33.419441'},
-        {'id': 594226727, 'state': 'CANCELED',
-         'date': '2018-09-12T21:27:25.241689'},
-        {'id': 939719570, 'state': 'EXECUTED',
-         'date': '2018-06-30T02:08:58.425572'}]
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
 
     assert sorting_dict_reverse(list_time_date, flag=False) == [
-        {'id': 939719570, 'state': 'EXECUTED',
-         'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED',
-         'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED',
-         'date': '2018-10-14T08:21:33.419441'},
-        {'id': 41428829, 'state': 'EXECUTED',
-         'date': '2019-07-03T18:35:29.512364'}]
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]

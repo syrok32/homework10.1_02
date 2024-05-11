a0073331be0043ba1c7e378f04a573ecb7f5dc82
Stepan <stepstepan2@gmail.com>
2024-05-11 13:06:32 +0300

    add mypy an type

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,7 +1,7 @@
 from src.processing import sorting_dict_reverse, sorting_dict_status
 
 
-def test_sorting_dict_status(list_sorting):
+def test_sorting_dict_status(list_sorting: list) -> None:
     assert sorting_dict_status(list_sorting, status="EXECUTED") == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -13,8 +13,8 @@ def test_sorting_dict_status(list_sorting):
 
 
 def test_sorting_dict_reverse(
-    list_time_date,
-):
+    list_time_date: list,
+) -> None:
     assert sorting_dict_reverse(list_time_date, flag=True) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
